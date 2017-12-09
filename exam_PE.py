3ba117b27434430cebe9044156fbf27d7eb7369d
zmdyy0318 <zmdyy0318@zmdyy0318deMacBook-Air.local>
2017-12-09 19:15:04 +0800

    随网站更新

--- exam_PE.py
+++ exam_PE.py
@@ -5,6 +5,7 @@ import sys
 import ans
 import time
 import json
+import base64
 import datetime
 import subprocess
 import requests
@@ -28,172 +29,128 @@ def check_network():
         raise Exception('error!')
 
 
-def parse_checkcode():
-    path_checkcode = os.path.join(os.getcwd(), 'checkcode.jpg')
-    r = session.get(url + '/getRandomPictrue.action')
-    f = open(path_checkcode, 'wb')
-    f.write(r.content)
-    f.close()
-    time.sleep(1)
-    if sys.platform.find('darwin') >= 0:
-        subprocess.call(['open', path_checkcode])
-    elif sys.platform.find('linux') >= 0:
-        subprocess.call(['xdg-open', path_checkcode])
-    else:
-        os.startfile(path_checkcode)
-    data = {
-        'vidPass': raw_input("Please input checkcode:")
-    }
-    os.remove(path_checkcode)
-    r = session.post(url + '/$%7BpageContext.request.contextPath%20%7D/chkVidPass.action', data)
-    if r.text == 'false':
-        return 'false'
-    else:
-        return data['vidPass']
-
-
-def parse_login(checkcode):
+def parse_login():
     data_login = {
-        'loginName': raw_input('Please input username:'),
-        'loginType': 'list_course.action',
-        'password': raw_input('Please input password:'),
-        'vidPass': checkcode
+        'username': base64.b64encode(raw_input('Please input username:')),
+        'password': base64.b64encode(raw_input('Please input password:')),
     }
-    r = session.post(url + '/stuLogin.action', data_login)
-    if r.text == '' or r.text.find('安全退出') >= 0:
-        return True
-    elif r.text.find('用户名或密码不正确') >= 0:
-        print('Username or password error!')
+    r = session.post(url + '/login/student', data_login)
+    j = json.loads(r.text)
+    if j["success"] is None:
+        print("Login error!")
         return False
     else:
-        return False
+        print(j["msg"]),
+        return j["success"]
 
 
 def get_info():
-    r = session.get(url + '/jsonStudent.action')
-    j = json.loads(r.text.replace('"', "").replace("'", '"'))
-    if j['userId'] == '':
-        sys.exit('error!')
-    else:
-        print('Welcome,' + j['userName'])
+    r = session.get(url + '/front/index')
+    soap = BeautifulSoup(r.text, "lxml")
+    print(soap.find(id="name").text)
+    return
 
 
-def get_exam():
-    r = session.get(url + '/selectExamCourse.action')
+def get_gotoexam():
+    r = session.get(url + '/front/exam/gotoExamPage')
     soup = BeautifulSoup(r.text, 'lxml')
-    span = soup.find_all('span')
-    scId = re.search('startExam\((.*?)\)', r.text).group(1)
-    if r.text.find('本学期无选课记录') >= 0:
-        exit('本学期无选课记录')
-    print(span[0].text.strip()),
-    print(span[1].text.strip())
-    return scId
+    span = soup.find('span')
+    print(span.text)
+    if soup.find(id="toExam") is None:
+        exit(0)
+    else:
+        return
 
 
-def get_answer(tihao, eselect):
-    try:
-        return ans.answer[int(tihao)]
-    except Exception, e:
-        if eselect.find('judAns') >= 0:
-            return '3'
-        else:
-            return 'E'
+def get_score():
+    r = session.get(url + '/front/club/updateScoreOnLine/100,5')
+    soup = BeautifulSoup(r.text, 'lxml')
+    span = soup.find_all('span')[1]
+    print(span.text)
 
 
-def parse_eselect(sc_id, flag_save):
+def parse_exam(flag_save):
     repeat = 1
-    r = session.get(url + '/startExam.action?scId='+sc_id+'&function%20random()%20{%20%20%20%20[native%20code]}')
+    r = session.get(url + '/front/exam/getExamByRandom/5')
     soup = BeautifulSoup(r.text, 'lxml')
-    token = soup.find(attrs={"name": "token"})['value']
-    tihao = soup.find_all(attrs={"name": "tihao"})
-    post_data = 'struts.token.name=token&token='+token+'&statrTime=statrTime&'
-    for i in range(0, 10):
-        post_data += 'tihao='+tihao[i]['value']+'&'
-        post_data += 'judAns['+str(i)+']='+get_answer(tihao[i]['value'], 'judAns')+'&'
-    for i in range(0, 20):
-        post_data += 'tihao='+tihao[i+10]['value']+'&'
-        post_data += 'sigAns['+str(i)+']='+get_answer(tihao[i+10]['value'], 'sigAns')+'&'
-    post_data += 'scId='+sc_id+'&'
-    for i in range(0, 10):
-        post_data += 'tihao='+tihao[i+30]['value']+'&'
-        post_data += 'mulAns['+str(i)+']='+get_answer(tihao[i+30]['value'], 'mulAns')+'&'
-    for i in range(0, 10):
-        post_data += 'tihao='+tihao[i+40]['value']+'&'
-        post_data += 'filAns['+str(i)+']='+get_answer(tihao[i+40]['value'], 'filAns')+'&'
-    post_data = post_data[:-1]
+    judNum = int(soup.find(id="judNum").attrs["value"])
+    sigNum = int(soup.find(id="sigNum").attrs["value"])
+    mulNum = int(soup.find(id="mulNum").attrs["value"])
+    filNum = int(soup.find(id="filNum").attrs["value"])
+    # yearTeamId = soup.find(id="yearTeamId").attrs["value"]
+    judAnsExamArr = soup.find_all(attrs={"name": re.compile("judAnsExam\[\d*\]")})
+    sigAnsExamArr = soup.find_all(attrs={"name": re.compile("sigAnsExam\[\d*\]")})
+    mulAnsExamArr = soup.find_all(attrs={"name": re.compile("mulAnsExam\[\d*\]")})
+    filAnsExamArr = soup.find_all(attrs={"name": re.compile("filAnsExam\[\d*\]")})
+    sigAnsArr = soup.find_all(attrs={"name": re.compile("sigAns\[\d*\]")})
+    mulAnsArr = soup.find_all(attrs={"name": re.compile("mulAns\[\d*\]")})
+    filAnsArr = soup.find_all(attrs={"name": re.compile("filAns\[\d*\]")})
+    judAnsDict = {"1": "正确", "2": "错误"}
     if flag_save is True:
-        n_pd = 0
-        n_dx = 0
-        n_ddx = 0
-        n_xc = 0
-        f_pd = open('%s %s pd.txt' % ('体育与健康基础理论知识', now), 'a+')
-        f_dx = open('%s %s dx.txt' % ('体育与健康基础理论知识', now), 'a+')
-        f_ddx = open('%s %s ddx.txt' % ('体育与健康基础理论知识', now), 'a+')
-        f_xc = open('%s %s xc.txt' % ('体育与健康基础理论知识', now), 'a+')
-        f_pd.seek(0)
-        f_dx.seek(0)
-        f_ddx.seek(0)
-        f_xc.seek(0)
-        r_pd = f_pd.read()
-        r_dx = f_dx.read()
-        r_ddx = f_ddx.read()
-        r_xc = f_xc.read()
-        for i in range(0, 10):
-            q = tihao[i].parent.find('span').text.strip()
-            if q not in r_pd:
+        n_jud = 0
+        n_sig = 0
+        n_mul = 0
+        n_fil = 0
+        f_jud = open('%s %s jud.txt' % ('体育与健康基础理论知识', now), 'a+')
+        f_sig = open('%s %s sig.txt' % ('体育与健康基础理论知识', now), 'a+')
+        f_mul = open('%s %s mul.txt' % ('体育与健康基础理论知识', now), 'a+')
+        f_fil = open('%s %s fil.txt' % ('体育与健康基础理论知识', now), 'a+')
+        f_jud.seek(0)
+        f_sig.seek(0)
+        f_mul.seek(0)
+        f_fil.seek(0)
+        r_jud = f_jud.read()
+        r_sig = f_sig.read()
+        r_mul = f_mul.read()
+        r_fil = f_fil.read()
+        for i in range(0, judNum):
+            q = judAnsExamArr[i].parent.find_all("span")[1].text.strip()
+            if q not in r_jud:
                 repeat = 0
-                n_pd = n_pd + 1
-                f_pd.write(tihao[i].attrs['value'] + '.' + q + '\n')
-        for i in range(0, 20):
-            q = tihao[i + 10].parent.find_all('span')[i].text.strip()
-            if q not in r_dx:
+                n_jud = n_jud + 1
+                f_jud.write(q + '\n')
+                f_jud.write("正确答案：" + judAnsDict[judAnsExamArr[i].attrs["value"]] + "\n")
+        for i in range(0, sigNum):
+            q = sigAnsExamArr[i].parent.find_all("span")[1].text.strip()
+            if q not in r_sig:
                 repeat = 0
-                n_dx = n_dx + 1
-                f_dx.write(tihao[i + 10].attrs['value'] + '.' + q + '\n')
-                f_dx.write(tihao[i + 10].parent.find_all('label')[i * 4 + 0].text.strip() + '\n')
-                f_dx.write(tihao[i + 10].parent.find_all('label')[i * 4 + 1].text.strip() + '\n')
-                f_dx.write(tihao[i + 10].parent.find_all('label')[i * 4 + 2].text.strip() + '\n')
-                f_dx.write(tihao[i + 10].parent.find_all('label')[i * 4 + 3].text.strip() + '\n')
-        for i in range(0, 10):
-            q = tihao[i + 30].parent.find_all('span')[i].text.strip()
-            if q not in r_ddx:
+                n_sig = n_sig + 1
+                f_sig.write(q + '\n')
+                f_sig.write(sigAnsArr[i * 4 + 0].parent.text.strip() + '\n')
+                f_sig.write(sigAnsArr[i * 4 + 1].parent.text.strip() + '\n')
+                f_sig.write(sigAnsArr[i * 4 + 2].parent.text.strip() + '\n')
+                f_sig.write(sigAnsArr[i * 4 + 3].parent.text.strip() + '\n')
+                f_sig.write("正确答案：" + sigAnsExamArr[i].attrs["value"] + '\n')
+        for i in range(0, mulNum):
+            q = mulAnsExamArr[i].parent.find_all("span")[1].text.strip()
+            if q not in r_mul:
                 repeat = 0
-                n_ddx = n_ddx + 1
-                f_ddx.write(tihao[i + 30].attrs['value'] + '.' + q + '\n')
-                f_ddx.write(tihao[i + 30].parent.find_all('label')[i * 4 + 0].text.strip() + '\n')
-                f_ddx.write(tihao[i + 30].parent.find_all('label')[i * 4 + 1].text.strip() + '\n')
-                f_ddx.write(tihao[i + 30].parent.find_all('label')[i * 4 + 2].text.strip() + '\n')
-                f_ddx.write(tihao[i + 30].parent.find_all('label')[i * 4 + 3].text.strip() + '\n')
-        for i in range(0, 10):
-            q = tihao[i + 40].parent.find_all('span')[i].text.strip()
-            if q not in r_xc:
+                n_mul = n_mul + 1
+                f_mul.write(q + '\n')
+                f_mul.write(mulAnsArr[i * 4 + 0].parent.text.strip() + '\n')
+                f_mul.write(mulAnsArr[i * 4 + 1].parent.text.strip() + '\n')
+                f_mul.write(mulAnsArr[i * 4 + 2].parent.text.strip() + '\n')
+                f_mul.write(mulAnsArr[i * 4 + 3].parent.text.strip() + '\n')
+                f_mul.write("正确答案：" + mulAnsExamArr[i].attrs["value"] + '\n')
+        for i in range(0, filNum):
+            q = filAnsExamArr[i].parent.find_all("span")[1].text.strip()
+            if q not in r_fil:
                 repeat = 0
-                n_xc = n_xc + 1
-                f_xc.write(tihao[i + 40].attrs['value'] + '.' + q + '\n')
-                f_xc.write(tihao[i + 40].parent.find_all('label')[i * 4 + 0].text.strip() + '\n')
-                f_xc.write(tihao[i + 40].parent.find_all('label')[i * 4 + 1].text.strip() + '\n')
-                f_xc.write(tihao[i + 40].parent.find_all('label')[i * 4 + 2].text.strip() + '\n')
-                f_xc.write(tihao[i + 40].parent.find_all('label')[i * 4 + 3].text.strip() + '\n')
-        f_pd.close()
-        f_dx.close()
-        f_ddx.close()
-        f_xc.close()
-        print('pd:' + str(n_pd)),
-        print('dx:' + str(n_dx)),
-        print('ddx:' + str(n_ddx)),
-        print('xc:' + str(n_xc)),
-    post_headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30',
-        'Referer': 'http://210.45.88.212/startExam.action?scId=' + sc_id + '&function%20random()%20%7B%20%20%20%20[native%20code]%7D'
-    }
-    r = session.post(url + '/stuExamResult.action', post_data, headers=post_headers)
-    soup = BeautifulSoup(r.text, 'lxml')
-    if flag_save is False:
-        print(soup.find(class_='courseRec').text),
-        print(soup.find('span').text)
-    else:
-        pass
+                n_fil = n_fil + 1
+                f_fil.write(q + '\n')
+                f_fil.write(filAnsArr[i * 4 + 0].parent.text.strip() + '\n')
+                f_fil.write(filAnsArr[i * 4 + 1].parent.text.strip() + '\n')
+                f_fil.write(filAnsArr[i * 4 + 2].parent.text.strip() + '\n')
+                f_fil.write(filAnsArr[i * 4 + 3].parent.text.strip() + '\n')
+                f_fil.write("正确答案：" + filAnsExamArr[i].attrs["value"] + '\n')
+        f_jud.close()
+        f_sig.close()
+        f_mul.close()
+        f_fil.close()
+        print('jud:' + str(n_jud)),
+        print('sig:' + str(n_sig)),
+        print('mul:' + str(n_mul)),
+        print('fil:' + str(n_fil))
     return repeat
 
 
@@ -202,15 +159,10 @@ def main():
     repeat = 0
     tolerance = None
     check_network()
-    while True:
-        checkcode = parse_checkcode()
-        if checkcode != 'false':
-            break
-        else:
-            print('Checkcode error!')
-    while parse_login(checkcode) is not True:
+    while parse_login() is not True:
         pass
     get_info()
+    get_gotoexam()
     a = raw_input('Catch question(This might take much time)[y/n]?')
     if a == 'y' or a == 'Y':
         flag_save = True
@@ -218,24 +170,21 @@ def main():
             tolerance = re.match('\d', raw_input('Plase input the tolerance[0-9]:'))
         tolerance = int(tolerance.group())
     else:
-        flag_save = False
-        tolerance = 0
-    sc_id = get_exam()
+        get_score()
     while repeat <= tolerance:
         if flag_save is True:
             num = num + 1
             print('Num:' + str(num)),
             print(str(repeat) + '<=' + str(tolerance)),
-            repeat_r = parse_eselect(sc_id, flag_save)
+            repeat_r = parse_exam(flag_save)
             if repeat_r == 0:
                 repeat = 0
             else:
                 repeat = repeat + repeat_r
-                print('success')
         elif flag_save is False:
-            repeat_r = parse_eselect(sc_id, flag_save)
+            repeat_r = parse_exam(flag_save)
             repeat = repeat + repeat_r
-    print('Done. All success')
+    print('Done. All success.')
 
 if __name__ == '__main__':
     main()
